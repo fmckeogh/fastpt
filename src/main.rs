b6afae8e398d6158b75b38bc90803a7b88924fe6
Ferdia McKeogh <ferdia@mckeogh.tech>
2024-11-11 19:56:30 +0000

    simulate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use libipt::{
     packet::{Packet, PacketDecoder},
-    ConfigBuilder,
+    ConfigBuilder, PtErrorCode,
 };
 use memmap2::MmapMut;
 use std::{env::args, fs::File, time::Instant};
@@ -17,18 +17,21 @@ fn main() {
     let mut data = unsafe { MmapMut::map_mut(&file) }
         .unwrap_or_else(|e| panic!("failed to map {path:?}: {e:?}"));
 
-    let mut decoder = PacketDecoder::new(&ConfigBuilder::new(&mut data).unwrap().finish()).unwrap();
-    decoder.sync_forward().unwrap();
-    assert_eq!(decoder.sync_offset().unwrap(), 0);
-
     let mut sum: u64 = 0;
+    let mut simulated_ringbuf_a = vec![];
+    let mut simulated_ringbuf_b = vec![];
+
     let start = Instant::now();
 
-    while let Ok(packet) = decoder.next() {
-        if let Packet::Ptw(inner) = packet {
-            sum = sum.wrapping_add(inner.payload());
-        }
-    }
+    data.chunks_mut(1024 * 1024 * 512).for_each(|c| {
+        simulated_ringbuf_a.extend_from_slice(c);
+
+        let read = decode(&mut sum, &mut simulated_ringbuf_a);
+
+        simulated_ringbuf_b.clear();
+        simulated_ringbuf_b.extend_from_slice(&simulated_ringbuf_a[read..]);
+        std::mem::swap(&mut simulated_ringbuf_a, &mut simulated_ringbuf_b);
+    });
 
     let end = Instant::now();
 
@@ -36,4 +39,21 @@ fn main() {
         "{:.2} GB/s, {sum}",
         (data.len() as f64) / ((end - start).as_nanos() as f64)
     );
+}
+
+fn decode(sum: &mut u64, buf: &mut [u8]) -> usize {
+    let mut decoder = PacketDecoder::new(&ConfigBuilder::new(buf).unwrap().finish()).unwrap();
+    decoder.sync_forward().unwrap();
+    assert_eq!(decoder.sync_offset().unwrap(), 0);
+
+    loop {
+        match decoder.next() {
+            Ok(Packet::Ptw(inner)) => *sum = sum.wrapping_add(inner.payload()),
+            Ok(_) => (),
+            Err(e) => match e.code() {
+                PtErrorCode::Eos => return dbg!(decoder.sync_offset().unwrap() as usize),
+                _ => panic!("{e:?}"),
+            },
+        }
+    }
 }
